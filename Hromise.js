4a807f18e6cc30579575cb2654d8aa22cd4158df
Hovhannes <hovhannesv@gmail.com>
2019-06-08 01:35:50 +0400

    adding Promise.all + bug fixes

--- Hromise.js
+++ Hromise.js
@@ -3,22 +3,31 @@ class Hromise {
         const self = this;
         this.isFulFilled = false;
         this.isRejected = false;
+        this.resolvedValue = null;
+        this.rejectError = null;
         this.resolveHandlers = [];
         this.rejectHandlers = [];
         setTimeout(function () {
             initial(resolve, reject);
             function resolve(val){
-                setTimeout(function() {
-                    self.isFulFilled = true;
-                    for (const handler of self.resolveHandlers)
-                        setTimeout(function () {
-                            handler(val);
-                        }, 0);
-                },0);
+                self.isFulFilled = true;
+                self.resolvedValue = val;
+                if(val instanceof Hromise)
+                    val.then(resolvedVal => invokeResolveHandlers(resolvedVal));
+                else
+                    setTimeout(() => invokeResolveHandlers(val),0);
+            }
+
+            function invokeResolveHandlers(val){
+                for (const handler of self.resolveHandlers)
+                    setTimeout(() => {
+                        handler(val);
+                    }, 0);
             }
             function reject(err) {
                 setTimeout(function() {
                     self.isRejected = true;
+                    self.rejectError = err;
                     for (const handler of self.rejectHandlers)
                         setTimeout(function () {
                             handler(err);
@@ -40,7 +49,7 @@ class Hromise {
             throw new Error("Handler has to be a function");
         const self = this;
         return new Hromise(function(resolve,reject) {
-            self.resolveHandlers.push(function(val,ex) {
+            const resolveHandler = (val,ex) => {
                 if(ex && val == null) return reject(ex);
                 try {
                     resolve(handler(val));
@@ -48,7 +57,14 @@ class Hromise {
                 catch(e){
                     reject(e);
                 }
-            })
+            };
+            if(!self.isFulFilled && !self.isRejected)
+                self.resolveHandlers.push(resolveHandler);
+            else
+                setTimeout(
+                    () => resolveHandler(self.resolvedValue, self.rejectError)
+                ,0)
+
         });
     }
     catch(handler) {
@@ -72,15 +88,31 @@ class Hromise {
             })
         });
     }
-    resolve(val){
-        return new Hromise(function(resolve, reject){
-            resolve(val);
-        });
-    }
-    reject(err){
-        return new Hromise(function(resolve, reject){
-            reject(err);
-        });
+    spread(handler){
+        return this.then(arr => handler(...arr));
     }
 }
+Hromise.resolve = (val) => {
+    return new Hromise(function(resolve, reject){
+        resolve(val);
+    });
+};
+Hromise.reject = (err) => {
+    return new Hromise(function(resolve, reject){
+        reject(err);
+    });
+};
+Hromise.all = (hromises) => {
+    if(!hromises || !Array.isArray(hromises))
+        throw new Error('Invalid arg');
+    const result = [];
+
+   return hromises
+       .reduce(
+            (acc, hromise) => acc.then(() => hromise).then(val => result.push(val)),
+            Hromise.resolve(1)
+        )
+        .then(() => result)
+
+};
 module.exports = Hromise;
